083c4c0fc50490cbef2d159ac33fe97fbb27c7d0
ctrly4sh <149458186+ctrly4sh@users.noreply.github.com>
2025-02-13 18:52:36 +0530

    api v1

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express"
+
+const app = express();
+
+app.get('/aws/health' , (req, res) => {
+    return res.send(`
+        <h1>Hello World</h1>
+        `)
+});
+
+app.listen(8001, ()=> {
+    console.log(`Server listening at localhost:8001`)
+})
